de7baab9967969806a4105692f0e858fc8717119
Jonathan <ozzhed@gmail.com>
2019-05-11 20:53:20 -0500

    detalles en diseño y parte del responsive listo. Siguiente: responsive del header y de servicios ofrecidos

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
+  animations: [
+    trigger('telefonoanimacion', [
+      state('show', style({
+        opacity: 100
+      })),
+      state('hide', style({
+        opacity: 0.8
+      })),
+      transition('show => hide', animate('500ms ease-out')),
+      transition('hide => show', animate('500ms ease-in')),
+    ])
+  ]
 })
+
 export class InfoComponent implements OnInit {
+
+  public telstate: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.telstate = "show";
+    setInterval(() => {
+      if(this.telstate == "show"){
+        this.telstate = "hide";
+      } else if(this.telstate == "hide") {
+        this.telstate = "show";
+      }
+    }, 500);
   }
-
 }
